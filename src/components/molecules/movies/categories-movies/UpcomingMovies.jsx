787c1e2d5f73cdff65115024765cec10b74fb4d9
yoram200 <yoram.hob@outlook.fr>
2021-11-09 12:13:19 +0100

    :fire: Add Input Search :fire:

--- src/components/molecules/movies/categories-movies/UpcomingMovies.jsx
+++ src/components/molecules/movies/categories-movies/UpcomingMovies.jsx
@@ -5,6 +5,7 @@ import { fetchUpcomingMovies } from "../../../../service/index";
 const UpcomingMovies = () => {
   const posterUrl = process.env.REACT_APP_POSTER_URL;
   const [upcoming, setUpcoming] = useState([]);
+  const [searchTerm, setSearchTerm] = React.useState("");
 
   useEffect(() => {
     const fetchAPI = async () => {
@@ -14,38 +15,52 @@ const UpcomingMovies = () => {
     fetchAPI();
   }, []);
 
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
     <>
       <div className="categories_movies">
         <div className="title">
           <h3>Upcoming Movies :</h3>
         </div>
+        <input
+          type="text"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={handleChange}
+        />
         <div className="all_infos">
           {upcoming.results
-            ? upcoming.results.map((movie, index) => {
-                return (
-                  <div key={index}>
-                    <Link to={`/movie/${movie.id}`}>
-                      <div
-                        style={{
-                          backgroundImage: `url(${
-                            posterUrl + movie.poster_path
-                          })`,
-                        }}
-                        className="container_img"
-                      ></div>
-                      <div className="container_title">
-                        <h3 className="movie_title">{movie.title}</h3>
-                        <h3 className="movie_rated">
-                          <span>Rated : </span>
-                          {movie.vote_average}
-                          <span className="note">/10</span>
-                        </h3>
-                      </div>
-                    </Link>
-                  </div>
-                );
-              })
+            ? upcoming.results
+                .filter((movie) => {
+                  return movie.title.includes(searchTerm);
+                })
+                .map((movie, index) => {
+                  return (
+                    <div key={index}>
+                      <Link to={`/movie/${movie.id}`}>
+                        <div
+                          style={{
+                            backgroundImage: `url(${
+                              posterUrl + movie.poster_path
+                            })`,
+                          }}
+                          className="container_img"
+                        ></div>
+                        <div className="container_title">
+                          <h3 className="movie_title">{movie.title}</h3>
+                          <h3 className="movie_rated">
+                            <span>Rated : </span>
+                            {movie.vote_average}
+                            <span className="note">/10</span>
+                          </h3>
+                        </div>
+                      </Link>
+                    </div>
+                  );
+                })
             : null}
         </div>
       </div>
